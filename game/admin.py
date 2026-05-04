1ae44d1c4b94232f7d7635f75bd4ea89e6c144dd
ds <148313992+dsit2324@users.noreply.github.com>
2026-05-04 20:10:11 +0200

    Delete game directory

--- game/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Player)
-admin.site.register(Card)
-admin.site.register(Battle)
-admin.site.register(Arena)
-admin.site.register(Clan)
-admin.site.register(PlayersCards)
--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('players/', views.players_list),
-    path('players/<int:id>/', views.player_detail),
-]

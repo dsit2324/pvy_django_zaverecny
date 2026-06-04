3629e60dda75254da90fbc08f858537c530c9ba0
ds <148313992+dsit2324@users.noreply.github.com>
2026-06-04 11:19:50 +0200

    Add files via upload

---
+++ game/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import CustomLoginView
+
+urlpatterns = [
+    path('', views.home),
+    path('players/', views.players_list),
+    path('players/<int:id>/', views.player_detail),
+    path('cards/', views.cards_list),
+    path('battles/', views.battles_list),
+    path('clans/', views.clans_list),
+    path('arenas/', views.arenas_list),
+    path("register/", views.register),
+    path("accounts/login/", CustomLoginView.as_view()),
+]
---
+++ game/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Player, Card, Battle, Clan, Arena
+from .forms import RegisterForm
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+    template_name = "registration/login.html"
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
+
+
+def home(request):
+    context = {
+        "players_count": Player.objects.count(),
+        "cards_count": Card.objects.count(),
+        "battles_count": Battle.objects.count(),
+    }
+    return render(request, "home.html", context)
+
+
+def players_list(request):
+    return render(request, "players.html", {"players": Player.objects.all()})
+
+
+def player_detail(request, id):
+    player = get_object_or_404(Player, player_id=id)
+    return render(request, "player_detail.html", {"player": player})
+
+
+def cards_list(request):
+    return render(request, "cards.html", {"cards": Card.objects.all()})
+
+
+def battles_list(request):
+    return render(request, "battles.html", {"battles": Battle.objects.all()})
+
+
+def clans_list(request):
+    return render(request, "clans.html", {"clans": Clan.objects.all()})
+
+
+def arenas_list(request):
+    return render(request, "arenas.html", {"arenas": Arena.objects.all()})
